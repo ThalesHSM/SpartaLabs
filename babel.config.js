753b09c6d9f304aca869b8ce4885bc804e3a5654
ThalesHSM <thsm271@gmail.com>
2021-09-23 20:08:52 -0300

    Project initialization, Testing, Eslint and Prettier setup

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,20 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
+    plugins: [
+      "@babel/plugin-proposal-export-namespace-from",
+      [
+        "module-resolver",
+        {
+          alias: {
+            "@src": "./src",
+            "@api": "./src/api",
+            "@screens": "./src/screens",
+            "@components": "./src/components",
+          },
+        },
+      ],
+    ],
   };
 };
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,10 +1,12 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
-import HomeScreen from "@screens/HomeScreen";
 
-export default function App() {
-  return <HomeScreen />;
+export default function HomeScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>AAA!</Text>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
